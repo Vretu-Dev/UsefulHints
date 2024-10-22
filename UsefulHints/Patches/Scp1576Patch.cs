52cbd9dcced41ad719969fb2fbb8fbdd49952369
Vretu-Dev <jakub.spieczny1@gmail.com>
2024-10-22 22:48:19 +0200

    patch updoot

--- UsefulHints/Patches/Scp1576Patch.cs
+++ UsefulHints/Patches/Scp1576Patch.cs
@@ -9,25 +9,19 @@ namespace UsefulHints.Patches
     [HarmonyPatch(typeof(Scp1576Item), nameof(Scp1576Item.ServerStopTransmitting))]
     public static class Scp1576StopTransmissionPatch
     {
+        private static Dictionary<Player, ItemType> activeItems = new Dictionary<Player, ItemType>();
         private static Dictionary<Player, CoroutineHandle> activeCoroutines = new Dictionary<Player, CoroutineHandle>();
 
         public static void Postfix(Scp1576Item __instance)
         {
             Player player = Player.Get(__instance.Owner);
 
-            if (activeCoroutines.ContainsKey(player))
+            if (activeCoroutines.ContainsKey(player) && activeItems.ContainsKey(player) && activeItems[player] == ItemType.SCP1576)
             {
                 Timing.KillCoroutines(activeCoroutines[player]);
                 activeCoroutines.Remove(player);
+                activeItems.Remove(player);
             }
-        }
-        public static void StartTransmissionCoroutine(Player player, CoroutineHandle coroutine)
-        {
-            if (activeCoroutines.ContainsKey(player))
-            {
-                Timing.KillCoroutines(activeCoroutines[player]);
-            }
-            activeCoroutines[player] = coroutine;
         }
     }
 }
