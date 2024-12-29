5f8d1f616de6df9eec2a2b99152f656fb3369a11
Vretu-Dev <jakub.spieczny1@gmail.com>
2024-12-29 22:10:35 +0100

    Include bc map fix

--- UsefulHints/EventHandlers/Modules/Maps.cs
+++ UsefulHints/EventHandlers/Modules/Maps.cs
@@ -1,32 +1,52 @@
 ﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Server;
 using MEC;
+using System.Collections.Generic;
 
 namespace UsefulHints.EventHandlers.Modules
 {
     public static class Maps
     {
+        private static CoroutineHandle broadcastCoroutine;
+
         public static void RegisterEvents()
         {
             Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += OnWaitingForPlayers;
         }
+
         public static void UnregisterEvents()
         {
             Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= OnWaitingForPlayers;
         }
+
         private static void OnRoundStarted()
         {
-            Timing.CallDelayed(445f, () =>
-            {
-                string message = UsefulHints.Instance.Config.BroadcastWarningLcz;
+            broadcastCoroutine = Timing.RunCoroutine(DelayedBroadcastCoroutine());
+        }
 
-                foreach (var player in Player.List)
+        private static void OnWaitingForPlayers()
+        {
+            Timing.KillCoroutines(broadcastCoroutine);
+        }
+
+        private static IEnumerator<float> DelayedBroadcastCoroutine()
+        {
+            yield return Timing.WaitForSeconds(445f);
+
+            if (Round.IsEnded)
+                yield break;
+
+            string message = UsefulHints.Instance.Config.BroadcastWarningLcz;
+
+            foreach (var player in Player.List)
+            {
+                if (player.IsAlive)
                 {
-                    if (player.IsAlive)
-                    {
-                        player.Broadcast(7, message);
-                    }
+                    player.Broadcast(7, message);
                 }
-            });
+            }
         }
     }
 }
