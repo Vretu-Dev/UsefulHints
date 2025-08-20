2d1a43a64bf07c5e7e9df530466e573710969283
Vretu-Dev <jakub.spieczny1@gmail.com>
2025-08-20 18:46:48 +0200

    Bump Exiled Version

--- UsefulHints/UsefulHints.cs
+++ UsefulHints/UsefulHints.cs
@@ -10,7 +10,7 @@ namespace UsefulHints
         public override string Name => "UsefulHints";
         public override string Author => "Vretu";
         public override string Prefix { get; } = "UH";
-        public override Version Version => new Version(3, 0, 1);
+        public override Version Version => new Version(3, 0, 2);
         public override Version RequiredExiledVersion { get; } = new Version(9, 6, 0);
         public override PluginPriority Priority { get; } = PluginPriority.Low;
         public static UsefulHints Instance { get; private set; }
